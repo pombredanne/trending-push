a4852d88d9172267dd8287a59a7a7295a979e6ea
Pawel Wawreszuk <p@wawr.eu>
2014-04-26 21:52:56 +0200

    update

--- app.go
+++ app.go
@@ -1,15 +1,19 @@
 package main
-/*TODO: 1) construct links in the message; 2) extract repo descriptions*/
+
+/*
+TODO: 
+- move pushover data to command line parameters
+*/
 
 import (
-  "fmt"
   "github.com/PuerkitoBio/goquery"
   "io/ioutil"
   "os"
   "encoding/json"
-  "strings"
   "net/http"
   "net/url"
+  "fmt"
+  "runtime"
 )
 
 func check(e error) {
@@ -18,32 +22,42 @@ func check(e error) {
   }
 }
 
+func homeDir() string {
+  if runtime.GOOS == "windows" {
+    home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+    if home == "" {
+      home = os.Getenv("USERPROFILE")
+    }
+    return home
+  }
+  return os.Getenv("HOME")
+}
+
 func main() {
-  dir, err := os.Getwd() 
-  check(err)
+  pushbulletToken := os.Args[1]
+  pushbulletDevice:= os.Args[2]
+  dir := homeDir() 
   oldItems := []string{}
-  if _, err := os.Stat(dir + "\\" + "old.json"); err == nil {
-    fmt.Print("if")
-    file, err := ioutil.ReadFile("old.json")
+  if _, err := os.Stat(dir + "/" + ".trending-push"); err == nil {
+    file, err := ioutil.ReadFile(dir + "/" + ".trending-push")
     check(err)
     json.Unmarshal(file, &oldItems)
-  } else {
-    fmt.Print("else")
   }
-  fmt.Print(oldItems)
 
   doc, err := goquery.NewDocument("https://github.com/trending")
   check(err)
   currentItems := []string{}
+  currentDescriptions:= []string{}
   doc.Find(".repo-leaderboard-list-item").Each(func(i int, s *goquery.Selection) {
     repo := s.Find("h2").Find(".repository-name")
+    description := s.Find(".repo-leaderboard-description")
     currentItems = append(currentItems, repo.Text())
-    fmt.Printf("repo: %s\n", repo.Text())
+    currentDescriptions = append(currentDescriptions, description.Text())
   })
-
   newItems := []string{}
+  newDescriptions := []string{}
   var isDuplicate bool
-  for _, currentValue := range currentItems {
+  for currentIndex, currentValue := range currentItems {
     isDuplicate = false
     for _, oldValue := range oldItems {
       if oldValue == currentValue {
@@ -53,17 +67,27 @@ func main() {
     }
     if isDuplicate == false {
       newItems = append(newItems, currentValue)
+      newDescriptions = append(newDescriptions,
+        currentDescriptions[currentIndex])
     }
   }
   jsonItems, _ := json.Marshal(append(oldItems, newItems...))
-  err = ioutil.WriteFile("old.json", jsonItems, 'l')
+  err = ioutil.WriteFile(dir + "/" + ".trending-push", jsonItems, 0666)
   check(err)
 
-  message := strings.Join(newItems, "\n\n")
-  _, err := http.PostForm("https://api.pushover.net/1/messages.json",
-    url.Values{
-      "token": {"aiCnXzdxjSYxjLdcba8aTmk7DVTKFZ"}, 
-      "user": {"ho8BieZry6ec5Ngft93AalCENwqxEk"}, 
-      "message": {message},
-    })
+  for index, value := range newItems {
+    message := "https://github.com/" + value + "\n" + 
+      newDescriptions[index]
+    res, err := http.PostForm("https://" + pushbulletToken +
+      "@api.pushbullet.com/api/pushes",
+      url.Values{
+        "device_iden": {pushbulletDevice}, 
+        "type": {"note"},
+        "title": {value},
+        "body": {message},
+      })
+    check(err)
+    fmt.Print(res)
+    fmt.Print("\n\n")
+  }
 }
